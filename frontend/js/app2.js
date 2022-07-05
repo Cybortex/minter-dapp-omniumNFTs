088f3b519db8cc298cdaf92d9496a885f42bbe72
Cybortex <olugbojiobafemi.com>
2022-07-05 19:57:03 +0100

    nmn

--- frontend/js/app2.js
+++ frontend/js/app2.js
@@ -25,3 +25,30 @@ function countdown2function() {
    
 setInterval(countdown2function, 1000);
 
+
+   
+const days22 = document.getElementById('days22')
+const hours22 = document.getElementById('hours22')
+const minutes22 = document.getElementById('minutes22')
+const seconds22 = document.getElementById('seconds22')
+
+const countdown2 = new Date().getTime();
+
+const countDownTime2 = new Date('August 01 2022 00:00:00');
+
+
+function countdown21function() {
+  const currentTime2 = new Date();
+  const diff2 = countDownTime2 - currentTime2;
+
+  const d2 = Math.floor(diff2 / 1000 / 60 / 60 / 24);
+  const h2 = Math.floor((diff2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const m2 = Math.floor((diff2 % (1000 * 60 * 60)) / (1000 * 60));
+  const s2 = Math.floor(diff2 / 1000) % 60;
+
+  days22.innerHTML = d2 < 10 ? '0' + d2: d2;
+  hours22.innerHTML = h2 < 10 ? '0' + h2 : h2;
+  minutes22.innerHTML = m2 < 10 ? '0' + m2 : m2;
+  seconds22.innerHTML = s2 < 10 ? '0' + s2 : s2;
+  }
+  setInterval(countdown21function, 1000);
