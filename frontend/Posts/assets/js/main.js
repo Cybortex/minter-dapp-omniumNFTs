67e474d9ea9f4469c144d7b972a2715fc3a777ad
Cybortex <olugbojiobafemi.com>
2022-10-04 02:37:03 +0100

    oct 2

--- frontend/Posts/assets/js/main.js
+++ frontend/Posts/assets/js/main.js
@@ -1,59 +1,38 @@
-const days2 = document.getElementById('days2')
-const hours2 = document.getElementById('hours2')
-const minutes2 = document.getElementById('minutes2')
-const seconds2 = document.getElementById('seconds2')
+  const days = document.getElementById('days')
+  const hours = document.getElementById('hours')
+  const minutes = document.getElementById('minutes')
+  const seconds = document.getElementById('seconds')
+  
+  const countdown = new Date().getTime();
+  
+  const countDownTime = new Date('October 15 2022 00:00:00');
+  
+  
+  function countdownfunction() {
+    const currentTime = new Date();
+    const diff = countDownTime - currentTime;
+  
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor(diff / 1000) % 60;
 
-const countdown = new Date().getTime();
+    if (diff < 0) {
+        days.innerHTML =  '00' 
+        hours.innerHTML =  '00' 
+        minutes.innerHTML = '00' 
+        seconds.innerHTML = '00' 
+        clearInterval(countdownfunction);
+      } else {
+    days.innerHTML = d < 10 ? '0' + d: d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+    }
+}setInterval(countdownfunction, 1000)
+  
 
-const countDownTime = new Date('August 28 2022 00:00:00');
-
-
-function countdown2function() {
-  const currentTime = new Date();
-  const diff = countDownTime - currentTime;
-
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const s = Math.floor(diff / 1000) % 60;
-
-  days2.innerHTML = d < 10 ? '0' + d: d;
-  hours2.innerHTML = h < 10 ? '0' + h : h;
-  minutes2.innerHTML = m < 10 ? '0' + m : m;
-  seconds2.innerHTML = s < 10 ? '0' + s : s;
-
-}
-   
-setInterval(countdown2function, 1000);
-
-
-   
-const days22 = document.getElementById('days22')
-const hours22 = document.getElementById('hours22')
-const minutes22 = document.getElementById('minutes22')
-const seconds22 = document.getElementById('seconds22')
-
-const countdown2 = new Date().getTime();
-
-const countDownTime2 = new Date('August 28 2022 00:00:00');
-
-
-function countdown21function() {
-  const currentTime2 = new Date();
-  const diff2 = countDownTime2 - currentTime2;
-
-  const d2 = Math.floor(diff2 / 1000 / 60 / 60 / 24);
-  const h2 = Math.floor((diff2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const m2 = Math.floor((diff2 % (1000 * 60 * 60)) / (1000 * 60));
-  const s2 = Math.floor(diff2 / 1000) % 60;
-
-  days22.innerHTML = d2 < 10 ? '0' + d2: d2;
-  hours22.innerHTML = h2 < 10 ? '0' + h2 : h2;
-  minutes22.innerHTML = m2 < 10 ? '0' + m2 : m2;
-  seconds22.innerHTML = s2 < 10 ? '0' + s2 : s2;
-  }
-  setInterval(countdown21function, 1000)
-// Grab elements
+  // Grab elements
 const selectElement = (selector) => {
     const element = document.querySelector(selector);
     if(element) return element;
@@ -76,7 +55,7 @@ window.addEventListener('scroll', scrollHeader);
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 const formOpenBtn = selectElement('#search-icon');
 const formCloseBtn = selectElement('#form-close-btn');
-const searchContainer = selectElement('#search-form-container');
+
 
 const toggleMenu = () =>{
     const mobileMenu = selectElement('#menu');
@@ -86,13 +65,7 @@ const toggleMenu = () =>{
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
-// Open/Close search form popup
-formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
-formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
-// -- Close the search form popup on ESC keypress
-window.addEventListener('keyup', (event) => {
-    if(event.key === 'Escape') searchContainer.classList.remove('activated');
-});
+
 
 // Switch theme/add to local storage
 const body = document.body;
